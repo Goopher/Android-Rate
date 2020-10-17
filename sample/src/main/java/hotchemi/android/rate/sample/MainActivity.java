d92e0e1dd20950bb227e7e1ac6b5699d04f6e9fb
Maijenburg, Goopher <Goopher.Maijenburg@adesso.eu>
2020-10-17 17:17:53 +0200

    Fixed isseus with latest version

--- sample/src/main/java/hotchemi/android/rate/sample/MainActivity.java
+++ sample/src/main/java/hotchemi/android/rate/sample/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends Activity {
                 .setShowLaterButton(true) // default true.
                 .setDebug(true) // default false.
                 .setCancelable(false) // default false.
+                .useInAppReviewSystem(true) // default false
                 .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
                     @Override
                     public void onClickButton(int which) {
